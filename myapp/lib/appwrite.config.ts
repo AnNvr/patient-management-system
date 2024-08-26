3f6337a00e443ba1b77d801e569d12fd098fe6b6
Andrea <an.nvr86@gmail.com>
2024-08-26 20:45:29 +1000

    update #2

--- myapp/lib/appwrite.config.ts
+++ myapp/lib/appwrite.config.ts
@@ -1,6 +1,5 @@
 import * as sdk from "node-appwrite";
 
-// Destructure environment variables
 export const {
   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
   PROJECT_ID,
@@ -12,30 +11,17 @@ export const {
   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
 } = process.env;
 
-// Check if the critical environment variables are defined
 if (!ENDPOINT) {
   throw new Error(
     "NEXT_PUBLIC_ENDPOINT is not defined. Check your .env.local file."
   );
 }
 
-if (!PROJECT_ID) {
-  throw new Error("PROJECT_ID is not defined. Check your .env.local file.");
-}
-
-if (!API_KEY) {
-  throw new Error("API_KEY is not defined. Check your .env.local file.");
-}
-
 const client = new sdk.Client();
 
-client
-  .setEndpoint(ENDPOINT) // Use the endpoint from the environment variable
-  .setProject(PROJECT_ID) // Use the project ID from the environment variable
-  .setKey(API_KEY); // Use the API key from the environment variable
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
-// Export initialized SDK modules
 export const databases = new sdk.Databases(client);
-export const storage = new sdk.Storage(client);
-export const messaging = new sdk.Messaging(client);
 export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
+export const storage = new sdk.Storage(client);
--- myapp/next.config.mjs
+++ myapp/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_ENDPOINT: process.env.NEXT_PUBLIC_ENDPOINT,
+    },
+    optimizeFonts: true,
+    reactStrictMode: true,
+};
 
 
 export default nextConfig;
